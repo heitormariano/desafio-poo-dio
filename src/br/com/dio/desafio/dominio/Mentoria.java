547fd1e6d8fe210f6ff70009c298ba24ff12af6e
heitormariano <heitormariano.camara@gmail.com>
2023-03-25 10:56:06 -0300

    definicao de entidades curso e mentoria

---
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -0,0 +1,53 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Mentoria {
+
+	private String titulo;
+	private String descricao;
+	LocalDate data;
+
+	public Mentoria() {
+		super();
+	}
+
+	public Mentoria(String titulo, String descricao, LocalDate data) {
+		this.titulo = titulo;
+		this.descricao = descricao;
+		this.data = data;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Mentoria [titulo=").append(titulo).append(", descricao=").append(descricao).append(", data=")
+				.append(data).append("]");
+		return builder.toString();
+	}
+
+}
